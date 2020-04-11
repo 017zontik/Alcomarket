cd82a5391cf7ca3cccf26f9ab8e63eda19f3a266
017zontik <017zontik@gmail.com>
2020-04-11 23:42:49 +0300

    class DataSourseUntil has been added

---
+++ src/com/tms/repositories/DataSourceUtil.java
@@ -0,0 +1,21 @@
+package com.tms.repositories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataSourceUtil {
+    private final static String url = "jdbc:mysql://localhost:3306/alcomarket";
+    private final static String user = "admin";
+    private final static String password = "171986";
+
+    public static  Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+    public static void closeConnection(Connection conn) throws SQLException {
+        conn.close();
+
+    }
+
+
+}
